09e56b4775277d7ae6399b14c194f2b5743c30d4
christyanlm <115588192+christyanlm@users.noreply.github.com>
2022-10-11 17:02:03 -0300

    Add files via upload
    fase inicial

---
+++ lm/js/x.js
@@ -0,0 +1,8 @@
+function clicar(){
+            if(document.querySelector(".menu nav ul").style.display == 'flex'){
+                document.querySelector(".menu nav ul").style.display == 'none';
+            } else{
+                document.querySelector(".menu nav ul").style.display =='flex';
+            }
+
+}
